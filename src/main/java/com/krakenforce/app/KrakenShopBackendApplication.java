623f9b9076f32b5ef056792e0d8871fe8cd901d3
toanngo2709 <ngotoan2709@outlook.com>
2021-10-16 08:08:30 +0700

    first commit

---
+++ src/main/java/com/krakenforce/app/KrakenShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.krakenforce.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrakenShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrakenShopBackendApplication.class, args);
+	}
+
+}
