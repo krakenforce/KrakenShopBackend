a876efb36c74dc802d414cf4c4b7a754a0bdbdab
toanngo2709 <ngotoan2709@outlook.com>
2021-10-19 09:46:43 +0700

    update security config & add dtos

--- src/main/java/com/krakenforce/app/dtos/BannerTypeDtos.java
+++ src/main/java/com/krakenforce/app/dtos/BannerTypeDtos.java
@@ -1,63 +1,49 @@
-package com.krakenforce.app.model;
+package com.krakenforce.app.dtos;
 
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import com.krakenforce.app.model.HomePageBanner;
 
-@Entity
-@Table(name = "banner_type")
-public class BannerType {
+public class BannerTypeDtos {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
 	private int id;
-	
-	@Column(name = "name")
 	private String name;
-	
-	@Column(name = "status")
 	private boolean status;
-	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "bannerType")
 	private Set<HomePageBanner> homePageBanners;
-
+	
+	public BannerTypeDtos() {
+		
+	}
+	
+	public BannerTypeDtos(int id, String name, boolean status, Set<HomePageBanner> homePageBanners) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.status = status;
+		this.homePageBanners = homePageBanners;
+	}
 	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public boolean isStatus() {
 		return status;
 	}
-
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
-
 	public Set<HomePageBanner> getHomePageBanners() {
 		return homePageBanners;
 	}
-
 	public void setHomePageBanners(Set<HomePageBanner> homePageBanners) {
 		this.homePageBanners = homePageBanners;
 	}
