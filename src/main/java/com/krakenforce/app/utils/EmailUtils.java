68b9360257f366c0ac3c65938a3169150ecd0885
toanngo2709 <ngotoan2709@outlook.com>
2021-11-14 19:57:19 +0700

    update

--- src/main/java/com/krakenforce/app/utils/EmailUtils.java
+++ src/main/java/com/krakenforce/app/utils/EmailUtils.java
@@ -1,14 +1,5 @@
 package com.krakenforce.app.utils;
 
-import java.io.UnsupportedEncodingException;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-
 public class EmailUtils {
 	
 	
